621b4f11c5885382d3747536558c528e2afd576e
priyankagrg645-ops <priyankagrg645@gmail.com>
2026-06-19 13:38:43 +0545

    added base contoller test

---
+++ tests/App/Controllers/baseController_test.py
@@ -0,0 +1,10 @@
+"""
+Test stub for App/Controllers/baseController.py
+
+Covers:
+    - BaseController.get_form_data()
+    - BaseController.is_logged_in()
+    - BaseController.get_current_user_id()
+    - BaseController.get_current_role()
+    - BaseController.flash_and_redirect()
+"""
