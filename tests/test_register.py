88ac5fab3b7350640973684cca79bd30429b401f
salina <salinnaashrestha67@gmail.com>
2026-06-18 22:58:10 -0700

    Update test_register.py

--- tests/test_register.py
+++ tests/test_register.py
@@ -1,10 +1,7 @@
 from app import create_app
 import pytest
 
-
-# =========================
 # FIXTURE
-# =========================
 @pytest.fixture
 def client():
     app = create_app()
@@ -13,31 +10,24 @@ def client():
     with app.test_client() as client:
         yield client
 
-# =========================
 # TEST 1: REGISTER PAGE LOADS
-# =========================
 def test_register_page_status(client):
     response = client.get("/register")
 
     assert response.status_code == 200
 
-# =========================
 # TEST 2: REGISTER FORM EXISTS
-# =========================
 def test_register_form_fields(client):
     response = client.get("/register")
-    html = response.data
+    html = response.data.lower()
 
     assert b"registration" in html
     assert b"username" in html
     assert b"email" in html
-    assert b"password" in html 
+    assert b"password" in html
 
-# =========================
 # TEST 3: LOGIN LINK EXISTS
-# =========================
 def test_login_link_exists(client):
     response = client.get("/register")
 
-    assert b"/login" in response.data
- 
+    assert b"login" in response.data.lower()
