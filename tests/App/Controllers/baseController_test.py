6412c200f3d601c95e92ab1d970a48d9d22a45df
Mohin25git <shakyamohingit25@gmail.com>
2026-06-19 13:41:56 +0545

    Added BaseControllerTest Functions

--- tests/App/Controllers/baseController_test.py
+++ tests/App/Controllers/baseController_test.py
@@ -8,3 +8,17 @@ Covers:
     - BaseController.get_current_role()
     - BaseController.flash_and_redirect()
 """
+import unittest
+
+class BaseControllerTest(unittest.TestCase):
+    """Tests for the shared BaseController helper methods."""
+
+    def setUp(self):
+        # TODO: instantiate BaseController() within an app/request context
+        pass
+
+    def test_get_form_data_returns_requested_fields(self):
+        # TODO: assert get_form_data(*fields) extracts only requested fields
+        # from request.form
+        pass
+    
