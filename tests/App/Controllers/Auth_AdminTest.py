a79da553e0968d67c30da3b836a3f60a67b70f87
Mohin25git <shakyamohingit25@gmail.com>
2026-06-19 12:53:51 +0545

    added tests App Controllers

---
+++ tests/App/Controllers/Auth_AdminTest.py
@@ -0,0 +1,25 @@
+"""
+Test stub for App/Controllers/Auth_Admin.py
+
+Covers:
+    - log_audit()
+    - send_notification()
+    - admin_required() decorator
+    - AuthController_Admin.login_admin()
+    - AuthController_Admin.logout_admin()
+    - AuthController_Admin.change_password_admin()
+    - AuthController_Admin.dashboard_admin()
+    - AuthController_Admin.api_activities()
+    - AuthController_Admin.api_activity_detail()
+    - AuthController_Admin.api_duplicate_activity()
+    - AuthController_Admin.api_bookings()
+    - AuthController_Admin.api_booking_detail()
+    - AuthController_Admin.api_payments()
+    - AuthController_Admin.api_verify_payment()
+    - AuthController_Admin.api_refund_payment()
+    - AuthController_Admin.api_manual_payment()
+    - AuthController_Admin.api_users()
+    - AuthController_Admin.api_user_detail()
+    - AuthController_Admin.api_notifications()
+    - AuthController_Admin.api_audit_logs()
+"""
---
+++ tests/App/Controllers/__init__Test.py
@@ -0,0 +1,18 @@
+"""
+Test stub for App/Controllers/__init__.py
+
+This module is currently empty (package marker only).
+"""
+import unittest
+
+
+class ControllersInitTest(unittest.TestCase):
+    """Tests for the Controllers package initializer."""
+
+    def test_package_imports_without_error(self):
+        # TODO: assert `import App.Controllers` succeeds without raising
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
