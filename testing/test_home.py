9fb3debaf1276344b0ffad9af3eddbaa85e74964
salina <salinnaashrestha67@gmail.com>
2026-06-18 22:06:45 -0700

    Add home page status test

--- testing/test_home.py
+++ testing/test_home.py
@@ -12,5 +12,12 @@ def client():
 
     with app.test_client() as client:
         yield client
+# =========================
+# TEST 1: HOME PAGE LOADS
+# =========================
+def test_home_page_status(client):
+    response = client.get("/")
 
-        
+    assert response.status_code == 200
+
+
