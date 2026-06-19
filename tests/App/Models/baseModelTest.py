641a2eb485f31f2e8c6ca2cefabee4360a7f91ff
Mohin25git <shakyamohingit25@gmail.com>
2026-06-19 13:55:42 +0545

    Added baseModelTest.py

---
+++ tests/App/Models/baseModelTest.py
@@ -0,0 +1,12 @@
+"""
+Test stub for App/Models/baseModel.py
+
+Covers:
+    - _safe_column()
+    - BaseModel.table (abstract property)
+    - BaseModel.find_by_id()
+    - BaseModel.find_by()
+    - BaseModel.find_all()
+    - BaseModel.count_all()
+    - BaseModel.delete_by_id()
+"""
